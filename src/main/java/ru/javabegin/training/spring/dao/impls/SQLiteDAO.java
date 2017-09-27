62fe01f31e71bdb7e541a5cff255319eecfb87bf
aromanchenkonn <a.romanchenko.nn@gmail.com>
2017-09-27 23:20:05 +0300

    add info lesson 34-spring dao

--- src/main/java/ru/javabegin/training/spring/dao/impls/SQLiteDAO.java
+++ src/main/java/ru/javabegin/training/spring/dao/impls/SQLiteDAO.java
@@ -1,15 +1,15 @@
 package ru.javabegin.training.spring.dao.impls;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import ru.javabegin.training.spring.dao.interfaces.MP3Dao;
@@ -18,77 +18,92 @@ import ru.javabegin.training.spring.dao.objects.MP3;
 @Component("sqliteDAO")
 public class SQLiteDAO implements MP3Dao {
 
-	private JdbcTemplate jdbcTemplate;
+	private NamedParameterJdbcTemplate jdbcTemplate;
 
 	@Autowired
 	public void setDataSource(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 	}
 
 	@Override
 	public void insert(MP3 mp3) {
-		String sql = "insert into mp3 (name, author) VALUES (?, ?)";
-		jdbcTemplate.update(sql, new Object[] { mp3.getName(), mp3.getAuthor() });
+		String sql = "insert into mp3 (name, author) VALUES (:name, :author)";
+
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("name", mp3.getName());
+		params.addValue("author", mp3.getAuthor());
+
+		jdbcTemplate.update(sql, params);
 	}
 
-	public void insertWithJDBC(MP3 mp3) {
-
-		Connection conn = null;
-
-		try {
-			Class.forName("org.sqlite.JDBC");
-			String url = "jdbc:sqlite:db/SpringDB.db";
-			conn = DriverManager.getConnection(url, "", "");
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
+	@Override
+	public void insert(List<MP3> mp3List) {
+		for (MP3 mp3 : mp3List) {
+			insert(mp3);
 		}
+	}
 
-		String sql = "insert into mp3 (name, author) VALUES (?, ?)";
+	@Override
+	public void delete(int id) {
+		String sql = "delete from mp3 where id=:id";
 
-		try {
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setString(1, mp3.getName());
-			ps.setString(2, mp3.getAuthor());
-			ps.executeUpdate();
-			ps.close();
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id", id);
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-
-		} finally {
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
+		jdbcTemplate.update(sql, params);
 	}
 
 	@Override
 	public void delete(MP3 mp3) {
-		// TODO Auto-generated method stub
-
+		delete(mp3.getId());
 	}
 
 	@Override
 	public MP3 getMP3ByID(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from mp3 where id=:id";
+
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id", id);
+
+		return jdbcTemplate.queryForObject(sql, params, new MP3RowMapper());
 	}
 
 	@Override
 	public List<MP3> getMP3ListByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from mp3 where upper(name) like :name";
+
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("name", "%" + name.toUpperCase() + "%");
+
+		return jdbcTemplate.query(sql, params, new MP3RowMapper());
 	}
 
 	@Override
 	public List<MP3> getMP3ListByAuthor(String author) {
-		// TODO Auto-generated method stub
-		return null;
+		String sql = "select * from mp3 where upper(author) like :author";
+
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("author", "%" + author.toUpperCase() + "%");
+
+		return jdbcTemplate.query(sql, params, new MP3RowMapper());
 	}
 
+	@Override
+	public int getMP3Count() {
+		String sql = "select count(*) from mp3";
+		return jdbcTemplate.getJdbcOperations().queryForObject(sql, Integer.class);
+	}
+
+	private static final class MP3RowMapper implements RowMapper<MP3> {
+
+		@Override
+		public MP3 mapRow(ResultSet rs, int rowNum) throws SQLException {
+			MP3 mp3 = new MP3();
+			mp3.setId(rs.getInt("id"));
+			mp3.setName(rs.getString("name"));
+			mp3.setAuthor(rs.getString("author"));
+			return mp3;
+		}
+
+	}
 }
--- src/main/java/ru/javabegin/training/spring/main/Start.java
+++ src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -3,23 +3,21 @@ package ru.javabegin.training.spring.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import ru.javabegin.training.spring.dao.impls.SQLiteDAO;
+import ru.javabegin.training.spring.dao.interfaces.MP3Dao;
 import ru.javabegin.training.spring.dao.objects.MP3;
 
 public class Start {
 
 	public static void main(String[] args) {
 		MP3 mp3 = new MP3();
-		mp3.setName("My Song name");
-		mp3.setAuthor("My Song author");
+		mp3.setName("Song name");
+		mp3.setAuthor("Song author");
 
 		// new SQLiteDAO().insertWithJDBC(mp3);
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-		SQLiteDAO sqLiteDAO = (SQLiteDAO) context.getBean("sqliteDAO");
+		MP3Dao sqLiteDAO = (MP3Dao) context.getBean("sqliteDAO");
 
-		sqLiteDAO.insert(mp3);
-
+		System.out.println(sqLiteDAO.getMP3ListByAuthor("Justin").size());
 	}
-
 }
